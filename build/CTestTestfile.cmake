d982cf085967d07442840fe32ae8255f08aa89a9
CoderAldrich <CoderAldrich@gmail.com>
2018-11-13 10:21:37 +0800

    VS2015编译

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/GitHub/hollows_hunter
+# Build directory: G:/GitHub/hollows_hunter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestRuns "hollows_hunter")
+subdirs("pe-sieve")
